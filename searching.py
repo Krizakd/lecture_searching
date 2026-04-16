dbcd242c9ab4772c57e0ef2fd1aaf7c6dd612424
krizakd63@gmail.com <krizakd63@gmail.com>
2026-04-16 15:44:22 +0200

    treti

--- searching.py
+++ searching.py
@@ -16,18 +16,39 @@ def read_data(file_name,klic):
 def linear_search(sekvence, cislo):
     count = 0
     seznam = []
+    slovnik = {}
     for poradi, i in enumerate(sekvence):
         if i == cislo:
             count += 1
             seznam.append(poradi)
-    return count, seznam
+            slovnik["positions"] = seznam
+            slovnik["count"] = count
+    return slovnik
+
+def binary_search(sekvence, cislo):
+    prava = len(sekvence) -1
+    leva = 0
+    while leva <= prava:
+        stred = (leva + prava) // 2
+        if sekvence[stred] == cislo:
+            return stred
+        elif sekvence[stred] < cislo:
+            leva = stred + 1
+        else:
+            prava = stred -1
+
+
+
+
 
 
 def main():
-    seqential=read_data("sequential.json","unordered_numbers")
-    pocet, poradi=linear_search(seqential, 0)
-    print(pocet)
-    print(poradi)
+    # seqential=read_data("sequential.json","unordered_numbers")
+    seqential = read_data("sequential.json", "ordered_numbers")
+
+    s=binary_search(seqential, 2)
+    print(s)
+
 
 
 
